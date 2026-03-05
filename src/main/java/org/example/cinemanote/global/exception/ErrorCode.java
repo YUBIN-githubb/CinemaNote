8a94f42edefbd3110f4db0b2bb4c9b844c604a00
YUBIN-githubb <yubin5744@gmail.com>
2026-03-05 21:28:04 +0900

    chore : 에러 핸들링 구현 #1

---
+++ src/main/java/org/example/cinemanote/global/exception/ErrorCode.java
@@ -0,0 +1,38 @@
+package org.example.cinemanote.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    // 인증/인가
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다"),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다"),
+
+    // 회원
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다"),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다"),
+
+    // 영화
+    MOVIE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 영화입니다"),
+    TMDB_API_ERROR(HttpStatus.BAD_GATEWAY, "영화 정보를 불러오는 데 실패했습니다"),
+
+    // 아카이브
+    ARCHIVE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 아카이브입니다"),
+    ARCHIVE_ACCESS_DENIED(HttpStatus.FORBIDDEN, "해당 아카이브에 접근 권한이 없습니다"),
+
+    // 단축 URL
+    SHORT_LINK_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 단축 링크입니다"),
+
+    // 공통
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 입력값입니다"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
