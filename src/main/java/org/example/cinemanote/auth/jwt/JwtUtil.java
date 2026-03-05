7fe4c51aaa9dbe3f52075073a7ccbdd5ece383b9
YUBIN-githubb <yubin5744@gmail.com>
2026-03-05 22:16:11 +0900

    feat : JWT 필터/유틸 생성, bycrpt 커맨드 추가 #3

---
+++ src/main/java/org/example/cinemanote/auth/jwt/JwtUtil.java
@@ -0,0 +1,65 @@
+package org.example.cinemanote.auth.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.example.cinemanote.global.common.UserRole;
+import org.example.cinemanote.global.exception.CustomException;
+import org.example.cinemanote.global.exception.ErrorCode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final long TOKEN_TIME = 30 * 60 * 1000L;
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String createToken(Long userId, String email, UserRole userRole) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(String.valueOf(userId)) //JWT의 sub(주제) 필드에 userId를 문자열로 저장
+                        .claim("email", email)
+                        .claim("userRole", userRole) //JWT의 payload에 이메일과 역할(Role)을 추가
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME)) //토큰의 만료 시간을 현재 시간 + 30분으로 설정
+                        .setIssuedAt(date) //토큰 발급일을 현재시간으로 설정
+                        .signWith(key, signatureAlgorithm) // 암호화 알고리즘으로 서명
+                        .compact();
+    }
+
+    public String substringToken(String tokenValue) {
+        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+            return tokenValue.substring(7);
+        }
+        throw new CustomException(ErrorCode.INVALID_TOKEN);
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
