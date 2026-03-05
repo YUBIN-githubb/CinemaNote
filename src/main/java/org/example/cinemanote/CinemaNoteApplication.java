41147d5f410e6057e8c0719077423438020d0fa7
YUBIN-githubb <yubin5744@gmail.com>
2026-03-05 20:59:32 +0900

    first commit

---
+++ src/main/java/org/example/cinemanote/CinemaNoteApplication.java
@@ -0,0 +1,13 @@
+package org.example.cinemanote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaNoteApplication.class, args);
+    }
+
+}
