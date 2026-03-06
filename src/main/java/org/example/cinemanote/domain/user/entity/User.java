6c7fab3a818e682e76990712b1f7840b3fc67774
YUBIN-githubb <yubin5744@gmail.com>
2026-03-06 21:12:30 +0900

    feat : 유저 엔티티에 userRole 추가 & 스태틱 생성 메서드 추가 #3

--- src/main/java/org/example/cinemanote/domain/user/entity/User.java
+++ src/main/java/org/example/cinemanote/domain/user/entity/User.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.cinemanote.global.common.BaseEntity;
+import org.example.cinemanote.global.common.UserRole;
 
 @Entity
 @Getter
@@ -25,10 +26,19 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String nickname;
 
-    private User(String email, String password, String nickname) {
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole userRole;
+
+    private User(String email, String password, String nickname, UserRole userRole) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
+        this.userRole = userRole;
+    }
+
+    public static User of(String email, String password, String nickname, UserRole userRole) {
+        return new User(email, password, nickname, userRole);
     }
 
     public void changeNickname(String newNickname) {
