7245092dc6137dea8eb3d77745daed03b98a0047
YUBIN-githubb <yubin5744@gmail.com>
2026-03-12 20:30:24 +0900

    fix : rating 유효성 검증 추가 #9

--- src/main/java/org/example/cinemanote/domain/archive/dto/request/ArchiveCreateRequest.java
+++ src/main/java/org/example/cinemanote/domain/archive/dto/request/ArchiveCreateRequest.java
@@ -1,5 +1,7 @@
 package org.example.cinemanote.domain.archive.dto.request;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -15,6 +17,8 @@ public class ArchiveCreateRequest {
     @NotNull
     private ContentType contentType;
 
+    @DecimalMin("0.0")
+    @DecimalMax("10.0")
     private float rating;
 
     @Size(max = 500)
