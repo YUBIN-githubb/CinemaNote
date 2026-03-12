d8c8a4ae76848bde7319ccf860976fd3abb3e98f
YUBIN-githubb <yubin5744@gmail.com>
2026-03-12 21:15:28 +0900

    fix : 업데이트 시에도 rating 유효성 검사 추가 #9

--- src/main/java/org/example/cinemanote/domain/archive/dto/request/ArchiveUpdateRequest.java
+++ src/main/java/org/example/cinemanote/domain/archive/dto/request/ArchiveUpdateRequest.java
@@ -1,11 +1,15 @@
 package org.example.cinemanote.domain.archive.dto.request;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class ArchiveUpdateRequest {
 
+    @DecimalMin("0.0")
+    @DecimalMax("10.0")
     private float rating;
 
     @Size(max = 500)
