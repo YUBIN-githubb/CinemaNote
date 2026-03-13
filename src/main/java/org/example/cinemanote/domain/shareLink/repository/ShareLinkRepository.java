c7a4b30303c96ae7987da87ae2e4f0494a63d1e1
YUBIN-githubb <yubin5744@gmail.com>
2026-03-13 21:08:38 +0900

    fix : 공유링크 조회 시 findByShareToken 메서드에서 유저정보를 함께 불러옴으로써 link.getUser() 시 +1 쿼리가 나가지 않도록 수정 #11

--- src/main/java/org/example/cinemanote/domain/shareLink/repository/ShareLinkRepository.java
+++ src/main/java/org/example/cinemanote/domain/shareLink/repository/ShareLinkRepository.java
@@ -3,10 +3,13 @@ package org.example.cinemanote.domain.shareLink.repository;
 import org.example.cinemanote.domain.shareLink.entity.ShareLink;
 import org.example.cinemanote.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ShareLinkRepository extends JpaRepository<ShareLink, Long> {
-    Optional<ShareLink> findByShareToken(String shareToken);
+    @Query("SELECT s FROM ShareLink s JOIN FETCH s.user WHERE s.shareToken = :shareToken")
+    Optional<ShareLink> findByShareToken(@Param("shareToken") String shareToken);
     Optional<ShareLink> findByUserAndIsActiveTrue(User user);
 }
